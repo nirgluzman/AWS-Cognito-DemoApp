69e9a9fb16a8dc2200c1c175665671385de54cb0
Nir Gluzman <nir.gluzman@gmail.com>
2025-01-29 23:51:25 +0100

    feat(auth): add a basic logIn function with Amplify

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,7 +1,12 @@
 import { Amplify } from 'aws-amplify';
 import amplifyConfig from '../amplifyConfig.json';
 
-// configure Amplify for Cognito authentication"
+import {
+  signIn, // sign-in a user
+  fetchAuthSession, // retrieve the current authentication session details
+} from 'aws-amplify/auth';
+
+// configure Amplify for Cognito authentication
 Amplify.configure({
   Auth: {
     Cognito: {
@@ -10,6 +15,25 @@ Amplify.configure({
     },
   },
 });
+
+async function logIn(username: string, password: string) {
+  const signInResult = await signIn({ username, password });
+  console.log(signInResult);
+  if (!signInResult.isSignedIn) {
+    throw new Error('User is not signed in');
+  }
+
+  const session = await fetchAuthSession({ forceRefresh: true });
+  console.log(session);
+  if (!session) {
+    throw new Error('No session found');
+  }
+
+  const idToken = session.tokens?.idToken?.toString();
+  const accessToken = session.tokens?.accessToken?.toString();
+
+  return { idToken, accessToken };
+}
 
 export async function test(element: HTMLButtonElement) {
   element.addEventListener('click', () => {
